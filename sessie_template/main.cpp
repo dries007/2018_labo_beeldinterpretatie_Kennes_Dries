49446d72982be3e5696f489a0ba23195483abdec
Dries007 <admin@dries007.net>
2018-10-21 17:02:50 +0200

    Template: Now in 3D!

---
+++ sessie_template/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @author Dries Kennes (R0486630)
+ */
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char **argv)
+{
+    CommandLineParser parser(argc, argv,
+                             "{ help h usage ?   |     | Show this massage. }"
+    );
+    parser.about("Parameters surrounded by <> are required.\n");
+
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+
+    return 0;
+}
