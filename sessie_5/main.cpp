c3b5e160f9d9952935b3d2e0098e41b094f70724
Dries007 <admin@dries007.net>
2018-11-30 22:55:10 +0100

    Skynet Awakes: Machine Learning, The Prequel Part II

--- sessie_5/main.cpp
+++ sessie_5/main.cpp
@@ -15,9 +15,7 @@ using namespace cv;
 void drawpoints();
 void mouseHandler(int event, int x, int y, int, void*);
 
-/**
- * C++, award winner for ugliest C-like syntax ever.
- */
+//C++, award winner for ugliest C-like syntax ever.
 class PointMouse: public Point {
 public:
     PointMouse(int x, int y, bool positive) : Point(x, y), positive(positive) { };
@@ -25,7 +23,75 @@ public:
 };
 
 Mat input_image;
+Mat hsv;
 vector<PointMouse> points;
+
+void show_masked(const String& name, const Mat& results)
+{
+    Mat mask = Mat::zeros(hsv.size(), CV_8UC1);
+    for (int r = 0; r < hsv.rows; r++)
+    {
+        for (int c = 0; c < hsv.cols; c++)
+        {
+            if (results.at<float>(r * hsv.cols + c) != 0)
+            {
+                mask.at<uchar>(r, c) = 1;
+            }
+        }
+    }
+
+    // Clear
+    erode(mask, mask, Mat(), Point(-1, -1), 2);
+    dilate(mask, mask, Mat(), Point(-1, -1), 2);
+
+    // Glue
+    dilate(mask, mask, Mat(), Point(-1, -1), 5);
+    erode(mask, mask, Mat(), Point(-1, -1), 5);
+
+    Mat masked;
+    input_image.copyTo(masked, mask);
+    imshow(name, masked);
+}
+
+void knn(const Mat& training, const Mat& labels, const Mat& samples)
+{
+    Ptr<ml::KNearest> knn = ml::KNearest::create();
+
+    knn->setIsClassifier(true);
+    knn->setAlgorithmType(ml::KNearest::Types::BRUTE_FORCE);
+    knn->setDefaultK(3);
+
+    knn->train(training, ml::ROW_SAMPLE, labels);
+
+    Mat results;
+    knn->findNearest(samples, knn->getDefaultK(), results);
+
+    show_masked("KNN Masked", results);
+}
+
+void nbc(const Mat& training, const Mat& labels, const Mat& samples)
+{
+    Ptr<ml::NormalBayesClassifier> nbc = ml::NormalBayesClassifier::create();
+
+    nbc->train(training, ml::ROW_SAMPLE, labels);
+    Mat results;
+    nbc->predict(samples, results);
+
+    show_masked("NBC Masked", results);
+}
+
+void svm(const Mat& training, const Mat& labels, const Mat& samples)
+{
+    Ptr<ml::SVM> svm = ml::SVM::create();
+    svm->setType(ml::SVM::C_SVC);
+    svm->setKernel(ml::SVM::LINEAR);
+    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->train(training, ml::ROW_SAMPLE, labels);
+    Mat results;
+    svm->predict(samples, results);
+
+    show_masked("SVM Masked", results);
+}
 
 int main(int argc, const char **argv)
 {
@@ -86,18 +152,17 @@ int main(int argc, const char **argv)
         }
     }
 
-    Mat hsv(input_image.clone());
+    // RGB (BGR) -> HSV
+    hsv = Mat(input_image.clone());
     // Remove green (BGR) -> 1e col
     // https://stackoverflow.com/q/23510571
-    for (int i = 0; i < input_image.rows; i++)
-    {
-        hsv.row(i).reshape(1, input_image.cols).col(1).setTo(Scalar(0));
-    }
+    for (int i = 0; i < input_image.rows; i++) hsv.row(i).reshape(1, input_image.cols).col(1).setTo(Scalar(0));
     cvtColor(hsv, hsv, COLOR_BGR2HSV);
 
     // Blur a little
     GaussianBlur(hsv, hsv, Size(5, 5), 0);
 
+    // Loop so adding points on the go is possible.
     do
     {
         drawpoints();
@@ -110,26 +175,25 @@ int main(int argc, const char **argv)
             }
         }
 
-        Mat training(points.size(), 3, CV_32FC1);
-        Mat labels(points.size(), 1, CV_32FC1);
+        // Everything below is meaningless (and breaks) if there are no points, so stop here.
+        if (points.size() == 0) continue;
 
+        // Training(Rows: 1 per point, Cols: HSV)
+        // Labels(Rows: 1 per point, Cols: Classifier (1 = yes, 0 = no))
+        Mat training(points.size(), 3, CV_32FC1);
+        Mat labels(points.size(), 1, CV_32SC1);
+
+        // Training data & labels.
         for (int i = 0; i < points.size(); i++)
         {
             Vec3b desc = hsv.at<Vec3b>(points[i]);
             training.at<float>(i, 0) = desc[0];
             training.at<float>(i, 1) = desc[1];
             training.at<float>(i, 2) = desc[2];
-            labels.at<float>(i, 0) = points[i].positive ? 1.0F : 0.0F;
+            labels.at<int>(i, 0) = points[i].positive ? 1 : 0;
         }
 
-        Ptr<ml::KNearest> knn = ml::KNearest::create();
-
-        knn->setIsClassifier(true);
-        knn->setAlgorithmType(ml::KNearest::Types::BRUTE_FORCE);
-        knn->setDefaultK(5);
-
-        knn->train(training, ml::ROW_SAMPLE, labels);
-
+        // You can't seem to do this any better way.
         Mat samples(hsv.rows * hsv.cols, 3, CV_32FC1);
         for (int r = 0; r < hsv.rows; r++)
         {
@@ -143,28 +207,9 @@ int main(int argc, const char **argv)
             }
         }
 
-        Mat knnResult;
-        knn->findNearest(samples, knn->getDefaultK(), knnResult);
-
-        Mat imgResult = Mat::zeros(hsv.size(), CV_8UC1);
-        for (int r = 0; r < hsv.rows; r++)
-        {
-            for (int c = 0; c < hsv.cols; c++)
-            {
-                int i = r * hsv.cols + c;
-
-                if (knnResult.at<float>(i))
-                {
-                    imgResult.at<uchar>(r, c) = 1;
-                }
-            }
-        }
-
-        erode(imgResult, imgResult, Mat::ones(2, 2, CV_8UC1));
-
-        Mat masked;
-        input_image.copyTo(masked, imgResult);
-        imshow("Mask", masked);
+        knn(training, labels, samples);
+        nbc(training, labels, samples);
+        svm(training, labels, samples);
     }
     while (waitKey(0) != '\x1B'); // Sleep & do event loop.
 
